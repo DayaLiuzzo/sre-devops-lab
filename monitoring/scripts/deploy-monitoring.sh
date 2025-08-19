819aa640db5591e2a4577dd530cec700d4f3cc2b
Daya Liuzzo <daya.liuzzo@gmail.com>
2025-08-19 16:17:03 +0200

    infra: reworked directory structure

--- monitoring/scripts/deploy-monitoring.sh
+++ monitoring/scripts/deploy-monitoring.sh
@@ -3,5 +3,3 @@ snap install helm --classic
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm upgrade --install prometheus prometheus-community/kube-prometheus-stack -n monitoring --create-namespace
-
-kubectl apply -f /home/vagrant/sre-lab/monitoring/servicemonitors/flask-app-servicemonitor.yaml
